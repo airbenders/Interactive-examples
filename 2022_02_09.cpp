c508677b1dd92691dc6da40824ebb90a51601eaa
Emirhan Arıkan <emirhan.16761@gmail.com>
2022-02-11 16:39:21 +0300

    adl ornegi eklendi

--- 2022_02_09.cpp
+++ 2022_02_09.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <string>
+
+namespace asd {
+    class bar {
+    public:
+        std::string toBePrinted = "Hello from namespace asd";
+    };
+    void print_adl(bar foo) {
+        std::cout << foo.toBePrinted << std::endl;
+    }    
+}
 
 int main(int argc, char *argv[]){
-	// adl arguement defined lookup 
+
+    print_adl(asd::bar()); // adl arguement defined lookup
     // std::array 
     // special member functions
     // smart pointers
     // operator overloading
     // smart pointers
     // std::copy, ostream_iterator
+    return 0;
 }
