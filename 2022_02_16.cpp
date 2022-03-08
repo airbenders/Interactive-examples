ad522d1661516d5edb747879f7c000a56b2f34a8
Erkam Kavak <erkam.kavak@boun.edu.tr>
2022-03-08 19:19:11 +0300

    iota, for_each, mem_fn example added

--- 2022_02_16.cpp
+++ 2022_02_16.cpp
@@ -6,6 +6,8 @@
 #include <algorithm>
 #include <functional>
 #include <chrono>
+#include <numeric>
+
 
 class Matrix_3{
 public:
@@ -21,6 +23,11 @@ public:
         row_3.push_back(v8);
         row_3.push_back(v9);
     }
+
+    Matrix_3(std::vector<int> values) : 
+    Matrix_3(values[0], values[1], values[2], 
+            values[3], values[4], values[5],    
+            values[6], values[7], values[8]) {}
 
     ~Matrix_3(){
         std::cout << row_1[0] << " " << row_1[1] << " " << row_1[2] << std::endl;
@@ -45,6 +52,17 @@ public:
         std::for_each(threads.begin(), threads.end(), std::mem_fn(&std::thread::join));
 
     }
+
+    friend std::ostream& operator<<(std::ostream &stream, const Matrix_3& mat)
+    {
+        for(auto i : mat.row_1) stream << i << " ";
+        stream << "\n";  
+        for(auto i : mat.row_2) stream << i << " "; 
+        stream << "\n";  
+        for(auto i : mat.row_3) stream << i << " ";
+        stream << "\n";  
+        return stream;
+    }
 private:
     void multiply_row(std::vector<int>& row, int c){
         std::for_each(row.begin(), row.end(), [&row, &c](int& number){
@@ -65,7 +83,7 @@ int main(){
         Matrix_3 matrix_2(1231, 4562, 7893, 2344, 5675, 8996, 3457, 6788, 9999);
 
         std::chrono::steady_clock::time_point begin_2 = std::chrono::steady_clock::now();
-        matrix_2.multiply_rows_threaded(117, 128, 139, 2);
+        // matrix_2.multiply_rows_threaded(117, 128, 139, 2);
         std::chrono::steady_clock::time_point end_2 = std::chrono::steady_clock::now();
         std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end_2 - begin_2).count() << "[ns]" << std::endl;
 
@@ -73,6 +91,27 @@ int main(){
         matrix_1.multiply_rows(117, 128, 139, 2);
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
         std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::nanoseconds> (end - begin).count() << "[ns]" << std::endl;
+    
+        auto values = [](auto initial_value) {
+            std::vector<int> v(9); 
+            std::iota(v.begin(), v.end(), initial_value); 
+            return v; 
+        };
+        Matrix_3 mat1(values(1)); 
+        Matrix_3 mat2(values(-10)); 
+        Matrix_3 mat3(values(10)); 
+        
+        //burada hepsini bir kez deconstruct ediyor nedenini anlamadım
+        std::vector<Matrix_3> matrices{mat1, mat2, mat3};
+
+        for(auto& mat: matrices) std::cout << mat << "\n"; 
+        auto multiply_row = std::mem_fn(&Matrix_3::multiply_rows); 
+        std::for_each(matrices.begin(), matrices.end(), [&] (auto& mat) {
+            multiply_row(mat, 5, 5, 5, 1); 
+        });
+        for(auto& mat: matrices) std::cout << mat << "\n"; 
+
+        std::cout << "end of program" << "\n"; 
     }
     catch(std::exception& e) {
         std::cout << e.what() << std::endl;
